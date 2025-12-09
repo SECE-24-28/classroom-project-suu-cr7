bb6da310dffe79aba031df2e215b482468a4a9f9
suu-cr7 <suvetha.s2024aids@sece.ac.in>
2025-12-09 11:28:48 +0530

    Add files via upload

---
+++ Day 6/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
+
+// import './App.css';
+// import Display from './components/Display';
+// function App() {
+ //  return (
+   // <div className="App">
+    // <h1> Welcome To React </h1>
+    // <Display/>
+    //</div>
+  // );
+//}
+
+//export default App;
---
+++ Day 6/src/components/Display.jsx
@@ -0,0 +1,6 @@
+function Display(){
+    return (
+        <div> React Leraning</div>
+    );
+}
+export default Display;
